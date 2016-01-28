f4c184f06e825002b64bce4870f167f08682b13e
cbaeza <carlos.baeza@posteo.de>
2016-01-28 22:49:04 +0100

    add basic angular material app

--- js/controller.js
+++ js/controller.js
@@ -5,11 +5,27 @@
 		'$scope',
 		'MyService',
 		function($scope, MyService){
-			console.log('controller init');
-			$scope.users = MyService.init();
 			
+			$scope.users = MyService.init();
+			$scope.menu = [
+				{ name:'table', avatar: 'svg-1', selected:false, content: 'table and filters'},
+				{ name:'other 1', avatar: 'svg-2', selected:false, content: 'some content 1'},
+				{ name:'other 2', avatar: 'svg-3', selected:false, content: 'some content 2'},
+				{ name:'other 3', avatar: 'svg-4', selected:false, content: 'some content 3'}
+			]
+			$scope.selected = $scope.menu[0];
+
 			$scope.init = function(){
-				
+				console.log('controller init !!');	
+			}
+
+			$scope.toggleList = function(){
+				console.log('toggleList');
+			}
+
+			$scope.selectItem = function ( item ) {
+				$scope.selected = angular.isNumber(item) ? $scope.menu[item] : item;
+				console.log($scope.selected)
 			}
 		}])
 }).call(this);
